d3b35d587fac34f5f311b25c165fe1e600fd3215
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-03-06 23:48:18 +0800

    feat: add aside keydown event

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,4 +5,5 @@
 ### Features
 
 01 index page set up ([f3da7af](https://github.com/Alejandrocsdev/jsPractice/commit/f3da7af4579bd6c72c084452b4f36caf07d996df))
-02 add aside click event ([](https://github.com/Alejandrocsdev/jsPractice/commit/))
+02 add aside click event ([0489ebf](https://github.com/Alejandrocsdev/jsPractice/commit/0489ebf5f88398796074e835ac3668aaa21a1c61))
+03 add aside keydown event ([](https://github.com/Alejandrocsdev/jsPractice/commit/))
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,14 @@
 // <<<DOM>>>
 const aside = document.querySelector('aside')
 const link = aside.querySelectorAll('.link')
+const section = document.querySelectorAll('section')
 
 function init() {
   // return to top when refresh
   document.body.scrollIntoView({ behavior: 'smooth', block: 'start' })
   // <<<EVENT LISTENER>>>
   aside.addEventListener('click', asideClick)
+  document.addEventListener('keydown', asideKey)
 }
 
 init()
@@ -21,4 +23,25 @@ function asideClick(element) {
     target.classList.toggle('clicked')
     sec.scrollIntoView({ behavior: 'smooth', block: 'start' })
   }
+}
+function asideKey(element) {
+  const last = section.length
+  const clicked = aside.querySelector('.clicked')
+  const id = clicked.dataset.id
+  let targetId = Number(id.split('-')[1])
+  if (element.key === 'ArrowDown' && clicked.dataset.id !== `sec-${last}`) {
+    targetId = `sec-${targetId + 1}`
+    keySwitch(clicked, targetId)
+  } else if (element.key === 'ArrowUp' && clicked.dataset.id !== 'sec-1') {
+    targetId = `sec-${targetId - 1}`
+    keySwitch(clicked, targetId)
+  }
+}
+// <<<OTHER FUNCTION>>>
+function keySwitch(clicked, targetId) {
+  clicked.classList.remove('clicked')
+  const targetLink = aside.querySelector(`[data-id="${targetId}"]`)
+  targetLink.classList.toggle('clicked')
+  const sec = document.querySelector(`#${targetId}`)
+  sec.scrollIntoView({ behavior: 'smooth', block: 'start' })
 }
