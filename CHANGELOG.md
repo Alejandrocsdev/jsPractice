d2f5bf5f94e6961cef2b9dd483ef915ff8c4d39c
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-03-07 00:08:50 +0800

    feat: add aside wheel event

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,5 +5,9 @@
 ### Features
 
 01 index page set up ([f3da7af](https://github.com/Alejandrocsdev/jsPractice/commit/f3da7af4579bd6c72c084452b4f36caf07d996df))
+
 02 add aside click event ([0489ebf](https://github.com/Alejandrocsdev/jsPractice/commit/0489ebf5f88398796074e835ac3668aaa21a1c61))
-03 add aside keydown event ([](https://github.com/Alejandrocsdev/jsPractice/commit/))
+
+03 add aside keydown event ([d3b35d5](https://github.com/Alejandrocsdev/jsPractice/commit/d3b35d587fac34f5f311b25c165fe1e600fd3215))
+
+04 add aside wheel event ([](https://github.com/Alejandrocsdev/jsPractice/commit/))
--- src/js/index.js
+++ src/js/index.js
@@ -9,14 +9,15 @@ function init() {
   // <<<EVENT LISTENER>>>
   aside.addEventListener('click', asideClick)
   document.addEventListener('keydown', asideKey)
+  document.addEventListener('wheel', asideWheel)
 }
 
 init()
 
 // <<<EVENT FUNCTION>>>
-function asideClick(element) {
+function asideClick(event) {
   link.forEach((e) => e.classList.remove('clicked'))
-  const target = element.target
+  const target = event.target
   const id = target.dataset.id
   const sec = document.querySelector(`#${id}`)
   if (target.classList.contains('link')) {
@@ -24,21 +25,35 @@ function asideClick(element) {
     sec.scrollIntoView({ behavior: 'smooth', block: 'start' })
   }
 }
-function asideKey(element) {
+function asideKey(event) {
+  if (event.key === 'ArrowDown') {
+    handleScroll('down')
+  } else if (event.key === 'ArrowUp') {
+    handleScroll('up')
+  }
+}
+function asideWheel(event) {
+  if (event.deltaY > 0) {
+    handleScroll('down')
+  } else if (event.deltaY < 0) {
+    handleScroll('up')
+  }
+}
+
+// <<<OTHER FUNCTION>>>
+function handleScroll(event) {
   const last = section.length
   const clicked = aside.querySelector('.clicked')
   const id = clicked.dataset.id
   let targetId = Number(id.split('-')[1])
-  if (element.key === 'ArrowDown' && clicked.dataset.id !== `sec-${last}`) {
+  if (event === 'down' && clicked.dataset.id !== `sec-${last}`) {
     targetId = `sec-${targetId + 1}`
-    keySwitch(clicked, targetId)
-  } else if (element.key === 'ArrowUp' && clicked.dataset.id !== 'sec-1') {
+  } else if (event === 'up' && clicked.dataset.id !== 'sec-1') {
     targetId = `sec-${targetId - 1}`
-    keySwitch(clicked, targetId)
   }
+  scroll(clicked, targetId)
 }
-// <<<OTHER FUNCTION>>>
-function keySwitch(clicked, targetId) {
+function scroll(clicked, targetId) {
   clicked.classList.remove('clicked')
   const targetLink = aside.querySelector(`[data-id="${targetId}"]`)
   targetLink.classList.toggle('clicked')
